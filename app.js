d3c4b0a18a01ddc18acbe9f5d7782e5f2a623251
Henry Lin <arkhenry@gmail.com>
2019-03-25 22:06:34 -0400

    get all endpoint

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const neo4j = require('neo4j-driver').v1;
 
 const app = express();
 const port = 3000;
-const uri = 'bolt://localhost:7687';
+const dbUri = 'bolt://localhost:7687';
 const user = 'neo4j';
 const password = 'password';
 
 app.get('/create_employee/:name/:id', (req, res) => {
-    const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+    const driver = neo4j.driver(dbUri, neo4j.auth.basic(user, password));
     const session = driver.session();
     const endMsg = `Created ${req.params.name}, ${req.params.id}`;
 
@@ -17,15 +17,27 @@ app.get('/create_employee/:name/:id', (req, res) => {
         {name: req.params.name, emp_id: req.params.id}
     ).then(() => {
         session.close(() => {
-            console.log(endMsg);
+            console.log(endMsg + ' yup');
         });
+        res.send(endMsg);
     });
-
-    res.send(endMsg);
 });
 
 app.get('/get_all_employees', (req, res) => {
-    const getQry = 'MATCH (e:Employee) RETURN e';
+    const driver = neo4j.driver(dbUri, neo4j.auth.basic(user, password));
+    const session = driver.session();
+    
+    session.run(
+        'MATCH (e:Employee) \
+        RETURN e.name AS name, e.emp_id as id'
+    ).then(result => {
+        session.close();
+        const resultJson = result.records.map(record => {
+            return {'name': record.get('name'), 'emp_id': record.get('id')};
+        });
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(resultJson));
+    });
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
